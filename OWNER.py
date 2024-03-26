f35f7a67ffe1c3f5f5f047f94229e1111255d4fc
ibra1410 <160061682+ibra1410@users.noreply.github.com>
2024-03-26 17:55:37 +0100

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
-OWNER = ["MeDoO15200", "MeDoO152"]
-OWNER_NAME = "ْ𓆩⧛ َََMeDoO   مــيــدو ⧚𓆪"
-BOT_TOKEN = "5343340817:AAFXfDoGDO19V9UECk3I6qaEIudoBgi2ueY"
+OWNER = ["X_G9E"]
+OWNER_NAME = "ibra"
+BOT_TOKEN = "6758851188:AAEkbAoj-zMFqtsYYhj5TBYv7UGqsn4qNMA"
 DATABASE = "mongodb+srv://medoohosam7:medoohosam7@cluster0.0wtskdg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-CHANNEL = "https://t.me/MeDoO15200"
-GROUP = "https://t.me/MeDoO1520"
-VIDEO = "https://t.me/V_l_B1"
-LOGS = "V_l_B2"
+CHANNEL = "https://t.me/MM_JJKK"
+GROUP = "https://t.me/MM_JJKK"
+VIDEO = "https://t.me/MM_JJKK"
+LOGS = "MM_JJKK"
 
